90f7a56fca114be64837a2928439ab42d48b9c21
Ram Dnyandev Gore <130224491+prover11@users.noreply.github.com>
2023-05-05 11:53:39 +0530

    Add files via upload

---
+++ find_grad_eg.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ int main()
+ {
+     int i;
+     printf("Enter the per of : ");
+     scanf("%d",&i);
+     switch(i)
+     {
+     case 100 ... 80:
+        {
+            printf("A grad");
+            break;
+        }
+     case 80 ...60:
+        {
+            printf("B grad");
+            break;
+        }
+     case 60 ...40:
+        {
+            printf("c grad");
+            break;
+        }
+     default:
+             {
+            printf("fail");
+             }
+     }
+ }
